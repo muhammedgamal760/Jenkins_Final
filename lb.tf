12c960adc53cb74d375024d7698487416de8803a
muhammedgamal760 <muhammedgamal760@gmail.com>
2022-06-10 03:01:46 +0200

    applying

--- lb.tf
+++ lb.tf
@@ -18,6 +18,19 @@ resource "aws_lb_target_group" "jimmy-target-group" {
   vpc_id   = module.network.vpc_id
 }
 
+resource "aws_lb_listener" "jimmy-listener" {
+  load_balancer_arn = aws_lb.jimmy-lb.arn
+  port              = "443"
+  protocol          = "TLS"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  alpn_policy       = "HTTP2Preferred"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.jimmy-target-group.arn
+  }
+}
+
 resource "aws_lb_target_group_attachment" "jimmy-target-group-attachment" {
   target_group_arn = aws_lb_target_group.jimmy-target-group.arn
   target_id        = aws_instance.ec2b.id
